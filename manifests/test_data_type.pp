878d9ca0260baf13bab44d26535fb1b28ff0ed82
egonzales28 <fgonzales@forsythe.com>
2017-06-04 16:00:30 -0400

    updating

---
+++ manifests/test_data_type.pp
@@ -0,0 +1,14 @@
+$test_var = $facts
+
+case $test_var {
+  Hash: {
+    notify { "This var is a hash": }
+  }
+  String: {
+    notify { "This var is a string!": }
+  }
+  default: {
+    notify { "Var not in list": }
+  }
+
+}
